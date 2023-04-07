12d4cd4683886d001aba64f23730664e02da4b58
mattmchugh4 <114327452+mattmchugh4@users.noreply.github.com>
2023-04-07 01:24:26 -0500

    Working version of comment analyzer

--- chatgpt-front-end/src/components/HttpCall.js
+++ chatgpt-front-end/src/components/HttpCall.js
@@ -2,22 +2,100 @@ import { useEffect, useState } from 'react';
 
 export default function HttpCall() {
   const [data, setData] = useState('');
+  const [input, setInput] = useState('');
 
-  useEffect(() => {
+  const handleRequest = () => {
     fetch('/http-call', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({ data: input }),
     })
       .then((response) => response.json())
       .then((responseData) => {
-        setData(responseData.data);
+        setData(responseData);
+        console.log(responseData);
       });
-  });
+  };
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const commentElement = data
+    ? data.comments.map((commentChain, chainIndex) => (
+        <div
+          key={chainIndex}
+          style={{
+            borderBottom:
+              chainIndex < data.comments.length - 1 ? '1px solid white' : '',
+            paddingBottom: '10px',
+            marginBottom: '10px',
+          }}
+        >
+          <div
+            style={{
+              color: 'red',
+            }}
+          >
+            {data.summaries[chainIndex] || null}
+          </div>
+          <div>
+            {commentChain.map((element, index) => {
+              const depthMatch = element.match(/(COMMENT|REPLY) (\d+)/);
+              const depth = depthMatch
+                ? depthMatch[1] === 'REPLY'
+                  ? parseInt(depthMatch[2])
+                  : 0
+                : 0;
+              const marginLeft = depth * 30; // Adjust the multiplier to control the indentation
+              return (
+                <div
+                  key={`${chainIndex}-${index}`}
+                  style={{
+                    marginTop: '10px',
+                    marginLeft: `${marginLeft}px`,
+                    color: 'white',
+                  }}
+                >
+                  {element}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))
+    : null;
+
   return (
     <>
-      <h2>HTTP Communication</h2>
-      <h3 className="http">{data}</h3>
+      <input type="text" onChange={handleInput} value={input} />
+      <div style={{ marginTop: '10px' }}>
+        <button onClick={handleRequest}>Request</button>
+      </div>
+      <h2>Response</h2>
+      {/* {data ? (
+        data.search_results.map((element, index) => (
+          <div key={index}>
+            <span>{element}</span>
+          </div>
+        ))
+      ) : (
+        <div />
+      )} */}
+      <div
+        style={{
+          display: 'flex',
+          alignContent: 'center',
+          justifyContent: 'center',
+          gap: '50px',
+        }}
+      >
+        <div style={{ backgroundColor: 'black', width: '700px' }}>
+          {commentElement}
+        </div>
+      </div>
     </>
   );
 }
